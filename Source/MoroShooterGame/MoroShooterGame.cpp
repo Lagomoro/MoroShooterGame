a8566e2ed34fcec1bb61e0b4a1bd7ee9b591107d
Lagomoro <2669708373@qq.com>
2020-12-10 13:06:30 +0800

    Initial commit

---
+++ Source/MoroShooterGame/MoroShooterGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoroShooterGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MoroShooterGame, "MoroShooterGame" );
+ 
